bfd02ebec23bb8ab2651918acbf0c5dec2bf60b0
Kirill Gritcenko <ssgone33@gmail.com>
2021-12-12 15:45:33 +0000

    Created theme, created .gitignore, included bootstrap

---
+++ wp-content/themes/movies-db/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+$cur_theme = wp_get_theme();
+
+define( 'MOVIES_DB_VERSION', $cur_theme->get( 'Version' ) );
+define( 'MOVIES_DB_TEXT_DOMAIN', $cur_theme->get( 'TextDomain' ) );
+
+function enqueue_assets() {
+	wp_enqueue_style( 'bootstrap', get_stylesheet_uri() . '/assets/deps/bootstrap/css/bootstrap.min.css', [], MOVIES_DB_VERSION );
+	wp_enqueue_script( 'bootstrap', get_stylesheet_uri() . '/assets/deps/bootstrap/js/bootstrap.js.css', [], MOVIES_DB_VERSION, true );
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_assets' );
