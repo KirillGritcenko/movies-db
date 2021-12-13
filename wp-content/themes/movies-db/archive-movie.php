747587c2d2ad53bfbf015e9b8d2452171a94321c
Kirill Gritcenko <ssgone33@gmail.com>
2021-12-13 08:46:47 +0000

    Added archive and single movie templates

--- wp-content/themes/movies-db/archive-movie.php
+++ wp-content/themes/movies-db/archive-movie.php
@@ -1,25 +1,29 @@
 <?php
 get_header();
 ?>
-    <div class="py-5 bg-light">
-        <div class="container">
-            <div class="row row-cols-3">
-				<?php while ( have_posts() ) : the_post(); ?>
-                    <div class="col">
-                        <div class="card h-100">
-							<?php the_post_thumbnail( 'large' ); ?>
-                            <div class="card-body">
-                                <h5 class="card-title"><?php the_title(); ?></h5>
-                                <p class="card-text"><?php the_excerpt(); ?></p>
-                            </div>
-                            <div class="card-footer">
-                                <a href="<?php echo get_permalink(); ?>" class="btn btn-primary"><?php echo __( 'Read more', MOVIES_DB_TEXT_DOMAIN ); ?></a>
-                            </div>
-                        </div>
-                    </div>
-				<?php endwhile; ?>
-            </div>
-        </div>
-    </div>
+	<div class="py-5">
+		<div class="row row-cols-4">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div class="col">
+					<div class="card h-100">
+						<?php the_post_thumbnail( 'large', [
+							'class' => 'img-fluid',
+						] ); ?>
+						<div class="card-body">
+							<h5 class="card-title">
+								<a class="text-black" href="<?php echo get_permalink(); ?>">
+									<?php the_title(); ?>
+								</a>
+							</h5>
+							<p class="card-text"><?php the_excerpt(); ?></p>
+						</div>
+						<div class="card-footer">
+							<a href="<?php echo get_permalink(); ?>" class="btn btn-primary"><?php echo __( 'Read more', MOVIES_DB_TEXT_DOMAIN ); ?></a>
+						</div>
+					</div>
+				</div>
+			<?php endwhile; ?>
+		</div>
+	</div>
 <?php
 get_footer();
