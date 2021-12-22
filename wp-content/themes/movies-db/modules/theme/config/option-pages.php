a9347488d87e2fb67fafe8f3c542bb3b408309f4
Kirill Gritcenko <ssgone33@gmail.com>
2021-12-22 11:41:12 +0000

    Added ability to register oprion pages

---
+++ wp-content/themes/movies-db/modules/theme/config/option-pages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	[
+		'page_title' => 'Movie DB General Settings',
+		'menu_title' => 'Movie DB Settings',
+		'menu_slug'  => 'movie-db-general-settings',
+		'capability' => 'edit_posts',
+		'redirect'   => false,
+	],
+];
